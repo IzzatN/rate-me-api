13027a43f27495d6866a637027d6626aad6b65e8
inugmanov <nugmanov.i.j@gmail.com>
2021-07-16 10:39:22 +0500

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  scope '/api' do
+    resources :categories, only: [:create, :index, :show]
+    resources :users, only: [:index, :create, :show, :update, :destroy]
+    resources :services, only: [:create, :index, :show, :update, :index]
+  end
+end
