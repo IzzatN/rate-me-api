9987d111949b5976f3a12d05dba550be75bce1d2
izzat <nugmanov.i.j@gmail.com>
2021-07-26 19:10:34 +0500

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,64 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+company = Company.create(
+  name: 'company 1',
+  short_name: 'test company',
+  description: 'dasda dasdasda ad',
+  logo_url: 'https://images.unsplash.com/photo-1472099645785-56…to=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  address: 'amir temur 48',
+  background_img_url: 'https://images.unsplash.com/photo-1472099645785-56…to=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  is_branch: false,
+  contact_phone: '132123123',
+  created_at: DateTime.now,
+  updated_at: DateTime.now,
+  parent_id: nil
+)
+
+# user1 = User.create(
+#   email: 'test@user.com',
+#   first_name: 'test',
+#   last_name: 'user',
+#   phone: '1321313',
+#   photoUrl: nil,
+#   is_consumer: false,
+#   created_at: DateTime.now,
+#   updated_at: DateTime.now
+# )
+
+# user2 = User.create(
+#   email: 'test@user2.com',
+#   first_name: 'test',
+#   last_name: 'user2',
+#   phone: '132131dsad3',
+#   photoUrl: nil,
+#   is_consumer: true,
+#   created_at: DateTime.now,
+#   updated_at: DateTime.now
+# )
+
+service1 = Service.create(
+  name: 'test 1',
+  description: 'dasda dasdasda ad',
+  created_at: DateTime.now,
+  updated_at: DateTime.now,
+  company_id: company.id
+)
+
+service2 = Service.create(
+  name: 'test 2',
+  description: 'dasda dasdasda ad',
+  created_at: DateTime.now,
+  updated_at: DateTime.now,
+  company_id: company.id
+)
+
+category1 = Category.create(
+  name: 'test 1',
+  value: 'test_1'
+)
+
+category2 = Category.create(
+  name: 'test 2',
+  value: 'test_2'
+)
+
+service1.categories << category1
+service2.categories << category2
