1b67d6e9fa9cb46b86c0655676c34ea57e04c5fd
izzat <nugmanov.i.j@gmail.com>
2021-08-23 19:22:55 +0500

    comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,27 +1,82 @@
 class CommentsController < ApplicationController
 
-    def index
-      comments = Comment.all
+  def index
+    comments = Comment.all
 
-      if params[:service_id]
-        comments = comments.where(service_id: params[:service_id])
-      end
-  
-      render json: CommentSerializer.new(comments).serializable_hash
+    if params[:service_id]
+      comments = comments.where(service_id: params[:service_id])
     end
-  
-    def show
-      comment = Comment.find(params[:id])
-  
-      if comment
-        render json: CommentSerializer.new(comment).serializable_hash
-      else
-        data = { errors: [status: 404, code: 'NotFound', detail: "Record not found"] }
-        render json: data, status: :not_found
-      end
-    end
-  
-    private
 
+    render json: CommentSerializer.new(comments).serializable_hash
   end
-  
+
+  def show
+    comment = Comment.find(params[:id])
+
+    if comment
+      render json: CommentSerializer.new(comment).serializable_hash
+    else
+      data = { errors: [status: 404, code: 'NotFound', detail: "Record not found"] }
+      render json: data, status: :not_found
+    end
+  end
+
+  def create
+    comment = Comment.new(create_params)
+
+    if comment.save
+      render json: CommentSerializer.new(comment).serializable_hash
+    else
+      data = { errors: [status: 422, code: 'UnprocessableEntity', details: comment.errors.messages] }
+      render json: data, status: :unprocessable_entity
+    end
+  end
+
+  # def update
+  #   comment = Comment.find(params[:id])
+
+  #   unless current_user.id == comment.user.id
+  #     return send_forbidden
+  #   end
+
+  #   if comment.update(create_params)
+  #     render json: CommentSerializer.new(comment).serializable_hash
+  #   else
+  #     data = { errors: [ status: 422, code: 'UnprocessableEntity', detail: comment.errors.messages ] }
+  #     render json: data, status: :unprocessable_entity
+  #   end
+  # end
+
+  # ishlavuradi
+  def destroy
+    comment = Comment.find(params[:id])
+
+    comment.destroy
+    render json: {}, status: :no_content
+  end
+
+  private
+
+  def create_params
+    comment = params[:data].require(:attributes).permit(
+      :text,
+      :user,
+      :service
+    )
+
+    if data = params.dig(:data, :relationships, :service, :data)
+      comment[:service_id] = data[:id]
+    end
+
+    if data = params.dig(:data, :relationships, :user, :data)
+      comment[:user_id] = data[:id]
+    end
+
+    comment
+  end
+end
+
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
     resources :companies, only: [:create, :index, :show, :update]
     resources :users, only: [:index, :create, :show, :update, :destroy]
     resources :services, only: [:create, :index, :show, :update, :index]
-    resources :comments, only: [:show, :index]
+    resources :comments, only: [:show, :index, :create, :destroy]
   end
 end
