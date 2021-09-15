4ff42a5434ee0ea2c99ddac5072ff3bf188156be
askarovDev <akhrorhoja@gmail.com>
2021-09-15 11:35:39 +0500

    db seed added new category and service

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+require 'database_cleaner/active_record'
+
+DatabaseCleaner.strategy = :truncation
+
+# then, whenever you need to clean the DB
+DatabaseCleaner.clean
+
 # Users ------------------------------
 
 yoshi = User.create(
@@ -54,39 +61,66 @@ autoritet = Company.create(
 vulkanizatsiya1 = Service.create(
   name: 'Vulkanizatsiya xizamtlari',
   description: 'Balon yamash, ...',
-  company_id: autoritet.id
+  company_id: autoritet.id,
+  updated_at: 5.days.ago.to_datetime,
+  created_at: 5.days.ago.to_datetime
 )
 
 remont_kuzova = Service.create(
   name: 'Kuzovnoy remont',
   description: 'Remont lyubix chastey kuzova',
-  company_id: autoritet.id
+  company_id: autoritet.id,
+  updated_at: 2.days.ago.to_datetime,
+  created_at: 2.days.ago.to_datetime
 )
 
-adinol = Service.create(
-  name: 'Moy almashtirish (Adinol)',
-  description: 'Adinol firmasining moy almashtirish xizmati',
-  company_id: autoritet.id
+addinol = Service.create(
+  name: 'Moy almashtirish (Addinol)',
+  description: 'Addinol firmasining moy almashtirish xizmati',
+  company_id: autoritet.id,
+  updated_at: 10.days.ago.to_datetime,
+  created_at: 10.days.ago.to_datetime
 )
 
 total = Service.create(
   name: 'Moy almashtirish (Total)',
   description: 'Total firmasining moy almashtirish xizmati',
-  company_id: autoritet.id
+  company_id: autoritet.id,
+  updated_at: 21.days.ago.to_datetime,
+  created_at: 21.days.ago.to_datetime
 )
 
 t_koreyskiy = Service.create(
   name: 'Krugovaya tonirokva',
   description: 'Tonirokva xizmati',
-  company_id: autoritet.id
+  company_id: autoritet.id,
+  updated_at: 12.days.ago.to_datetime,
+  created_at: 12.days.ago.to_datetime
 )
 
 t_mestniy = Service.create(
   name: 'Tonirovka Sobiraka',
   description: 'Tonirokva xizmati',
-  company_id: autoritet.id
+  company_id: autoritet.id,
+  updated_at: 12.days.ago.to_datetime,
+  created_at: 12.days.ago.to_datetime
 )
 
+liqui_moly = Service.create(
+  name: 'Moy almashtirish (Liqui Moly)',
+  description: 'Liqui Moly firmasining moy almashtirish xizmati',
+  company_id: autoritet.id,
+  updated_at: 3.days.ago.to_datetime,
+  created_at: 3.days.ago.to_datetime
+)
+
+mohir_fara = Service.create(
+  name: 'Restavraciya fara (Mohir Fara)',
+  description: 'Chistka, restavraciya, tuning fara',
+  company_id: autoritet.id,
+  updated_at: 8.days.ago.to_datetime,
+  created_at: 8.days.ago.to_datetime
+)
 # Categories ------------------------------
 
 shinomontaj = Category.create(
@@ -114,11 +148,25 @@ tonirovka = Category.create(
   value: 'tonirovka'
 )
 
+moyka = Category.create(
+  name: 'Moyka',
+  value: 'moyka'
+)
+
+elektrik = Category.create(
+  name: 'Elektrik',
+  value: 'elektrik'
+)
+
+
 # Categories/Services ------------------------------
 
 vulkanizatsiya1.categories << shinomontaj
 remont_kuzova.categories << kostoprav
-adinol.categories << moy_almashtirish
+remont_kuzova.categories << kostoprav
+mohir_fara.categories << elektrik
+addinol.categories << moy_almashtirish
+liqui_moly.categories << moy_almashtirish
 total.categories << moy_almashtirish
 t_koreyskiy.categories << tonirovka
 t_mestniy.categories << tonirovka
